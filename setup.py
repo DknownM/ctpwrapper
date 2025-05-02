bc3643d1648acfbfb9ff8c64551516746d91f3df
winton <365504029@qq.com>
2025-05-02 16:28:17 +0800

    fix warning

--- setup.py
+++ setup.py
@@ -120,8 +120,6 @@ setup(
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
-        "License :: OSI Approved",
-        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
         "Operating System :: POSIX",
         "Operating System :: Microsoft",
         "Operating System :: Microsoft :: Windows",
