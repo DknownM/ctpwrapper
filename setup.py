ef61a661fd0d61c6a7dde5d8528f6a52a0d7ba7b
winton <365504029@qq.com>
2026-06-08 18:35:08 +0800

    add

--- setup.py
+++ setup.py
@@ -94,8 +94,6 @@ ext_modules = [
 ]
 
 setup(
-    long_description=open("README.md", encoding="utf-8").read(),
-    long_description_content_type='text/markdown',
     include_dirs=[header_dir, cpp_header_dir],
     platforms=["win32", "linux"],
     packages=["ctpwrapper"],
